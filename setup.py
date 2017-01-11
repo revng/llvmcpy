3f51408e63a0faf6ea5f6a1fcffea5976eab18eb
Alessandro Di Federico <ale+llvmcpy@clearmind.me>
2017-01-11 11:42:15 +0100

    Update version number to 0.1.1

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='llvmcpy',
-    version='0.1.0',
+    version='0.1.1',
     description='Python bindings for LLVM auto-generated from the LLVM-C API',
     long_description=long_description,
     url='https://rev.ng/llvmcpy',
