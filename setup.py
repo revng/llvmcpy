385b5fffb6b20fd6ceaefcef5c40885202413417
Alessandro Di Federico <ale@rev.ng>
2024-07-05 16:01:37 +0200

    Update README.md

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
@@ -13,15 +13,15 @@ setup(
     version='0.1.5',
     description='Python bindings for LLVM auto-generated from the LLVM-C API',
     long_description=long_description,
-    url='https://rev.ng/llvmcpy',
+    long_description_content_type='text/markdown',
+    url='https://github.com/revng/llvmcpy',
     author='Alessandro Di Federico',
-    author_email='ale+llvmcpy@clearmind.me',
+    author_email='ale.llvmcpy@clearmind.me',
     license='MIT',
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 3',
     ],
     keywords='llvm',
