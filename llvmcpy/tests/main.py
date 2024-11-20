2de4a34cc32effb6465f5bbffabb9ef9121ac1ce
Giacomo Vercesi <giacomo.vercesi@rev.ng>
2024-11-20 16:33:14 +0100

    Add test for concurrent LLVM usage
    Add a test which leverages the capabilities of using multiple LLVM
    installations at the same time in the same python process.

--- llvmcpy/tests/main.py
+++ llvmcpy/tests/main.py
@@ -1,47 +1,10 @@
 import unittest
 
 from llvmcpy import LLVMCPy
-
-module_source = """; ModuleID = 'example.c'
-target datalayout = "e-m:e-i64:64-f80:128-n8:16:32:64-S128"
-target triple = "x86_64-pc-linux-gnu"
-
-; Function Attrs: nounwind uwtable
-define i32 @function2() {
-  ret i32 42
-}
-
-; Function Attrs: nounwind uwtable
-define i32 @function1() {
-  %1 = call i32 @function2()
-  %2 = call i32 @function2()
-  ret i32 %1
-}
-
-; Function Attrs: nounwind uwtable
-define i32 @main(i32, i8**) {
-  %3 = alloca i32, align 4
-  %4 = alloca i32, align 4
-  %5 = alloca i8**, align 8
-  store i32 0, i32* %3, align 4
-  store i32 %0, i32* %4, align 4
-  store i8** %1, i8*** %5, align 8
-  %6 = call i32 @function1()
-  ret i32 %6
-}
-"""
+from . import get_module_source
 
 llvm = LLVMCPy()
-llvm_major = int(llvm.version.split(".", 1)[0])
-
-if llvm_major >= 7:
-    module_source = (
-        module_source
-        + """
-    !llvm.module.flags = !{!0}
-    !0 = !{ i32 4, !"foo", i32 42 }
-    """
-    )
+module_source = get_module_source(llvm)
 
 
 def load_module(ir):
@@ -101,7 +64,7 @@ class TestSuite(unittest.TestCase):
         self.assertEqual(value.get_md_string(encoding=None), string.encode("ascii"))
 
     def test_metadata_flags(self):
-        if llvm_major < 7:
+        if int(llvm.version.split(".", 1)[0]) < 7:
             return
         module = load_module(module_source)
         length = llvm.ffi.new("size_t *")
